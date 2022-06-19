9584d22e1e428f9e8f583a00a70e6501fb2d9c40
KarthihaKrishnan <karthiha.au@gmail.com>
2022-06-19 19:18:35 +0930

    after solving vendor e

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,17 +1,14 @@
-/*package routes
+package routes
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/karthihakrishnan/checkoutservice/internal/models"
-
 	"github.com/gorilla/mux"
 )
 
 var RegisterProductRoutes = func(router *mux.Router) {
 	// Route handle & endpoints
 	//router.HandleFunc("/", homelink)
-	router := gin.Default()
-	router.HandleFunc("/product", models.FuncCreateNewProduct).Methods("POST")
+	//router := gin.Default()
+	//router.HandleFunc("/product", models.FuncCreateNewProduct).Methods("POST")
 	//	router.HandleFunc("/products", models.FuncGetAllProducts).Methods("GET")
 	//router.HandleFunc("/products/{id}", FuncgetOneProduct).Methods("GET")
 	//router.HandleFunc("/products/{id}", FuncupdateProduct).Methods("PATCH")
@@ -20,4 +17,3 @@ var RegisterProductRoutes = func(router *mux.Router) {
 	//router.Handle("/product/google", controllers.FuncGoogleHomesPromotion(db)).Methods("GET")
 	//router.Handle("/product/alexa", controllers.FuncAlexaSpeakersPromotion(db)).Methods("GET")
 }
-*/
